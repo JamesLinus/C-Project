ef3b4f89b134c768c45673ebb7ddb64537e651d0
fabien <fpoggi@student.42.fr>
2015-03-21 18:28:38 +0100

    libft

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -1,7 +1,19 @@
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t	n)
+void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, 0, n);
+	size_t	i;
+	char	*tmp;
+
+	tmp = (char*) s;
+	i = 0;
+	if (n == '0')
+		return ;
+	while (i < n)
+	{
+		tmp[i] = 0;
+		i++;
+	}
+	return ;
 }

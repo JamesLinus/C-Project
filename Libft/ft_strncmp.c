6bdc42abd91a592ff8279ee9db9c1dcf015b0b8f
fabien <fpoggi@student.42.fr>
2015-03-21 18:46:15 +0100

    libft

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -1,30 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fpoggi <fpoggi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/02/27 13:32:17 by fpoggi            #+#    #+#             */
-/*   Updated: 2015/03/16 13:28:36 by fpoggi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char *ps1;
-	unsigned char *ps2;
+	unsigned int	i;
 
-	ps1 = (unsigned char *)s1;
-	ps2 = (unsigned char *)s2;
-	while (n--)
-	{
-		if (*ps1 != *ps2)
-			return (*ps1 - *ps2);
-		ps1++;
-		ps2++;
-	}
-	return (0);
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n)
+		i++;
+	return (s1[i] - s2[i]);
 }

62e3990c029c06d984ec9eececb091445d874372
fabien <fpoggi@student.42.fr>
2015-03-21 18:33:43 +0100

    libft

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -1,29 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fpoggi <fpoggi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/02/27 10:42:32 by fpoggi            #+#    #+#             */
-/*   Updated: 2015/03/16 12:01:37 by fpoggi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	char			*pb;
-	unsigned char	car;
-	
-	if (b == NULL);
+	size_t	i;
+	char	*tmp;
+
+	if (b == NULL)
 		return (NULL);
-	pb = b;
-	car = (unsigned char)c;
-	while (len--)
+	tmp = (char *) b;
+	i = 0;
+	while (i < len)
 	{
-		*pb++ = car;
+		tmp[i] = (char) c;
+		i++;
 	}
 	return (b);
 }
